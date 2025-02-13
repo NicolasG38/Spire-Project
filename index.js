cad9f0431e196f279668cc2902a8c82b99ffbef9
NicolasG38 <53898852+NicolasG38@users.noreply.github.com>
2025-02-13 08:52:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const container = document.getElementById("container-content")
+let nanoSuitTitle=document.getElementById("nano-suit-title")
+const btnAliceEnsoleillee=document.getElementById("alice_ensoleillée")
+const btnAventuriereDesTerresDésolée=document.getElementById("aventuriere_des_terres_desolee")
+const btnBikiniDePlongeeVague=document.getElementById("bikini_de_plongee_vague")
+const btnBondageCybernétique=document.getElementById("bondage_cybernétique")
+const btnChaperonRougeEnsoleillee=document.getElementById("chaperon_rouge_ensoleillee")
+const btnCombinaisonDExplorationDOrca=document.getElementById("combinaison_d-exploration_d-orca")
+const btnCombinaisonDePlongeePlanetaire2=document.getElementById("combinaison_de_plongee_planetaire_2")
+const btnCombinaisonDePlongeePlanetaire2V2=document.getElementById("combinaison_de_plongee_planetaire_2-2")
+const btnCombinaisonDePlongeePlanetaire3=document.getElementById("combinaison_de_plongee_planetaire_3")
+const btnCombinaisonDePlongeePlanetaire3V2=document.getElementById("combinaison_de_plongee_planetaire_3-2")
+const nanoSuit=["IMG_001.png","IMG_002.png","IMG_003.png","IMG_004.png","IMG_005.png","IMG_006.png","IMG_007.png","IMG_008.png","IMG_008.png"]
+
+
+
+
+
+
